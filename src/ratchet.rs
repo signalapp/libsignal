ecaa63cb2f0c0835525f9a0bcb0e448caef7cf55
Jack Lloyd <jack@signal.org>
2020-07-07 19:51:17 -0400

    Remove unused imports

--- src/ratchet.rs
+++ src/ratchet.rs
@@ -3,8 +3,7 @@ mod params;
 
 pub use self::keys::{ChainKey, MessageKeys, RootKey};
 pub use self::params::{AliceSignalProtocolParameters, BobSignalProtocolParameters};
-use crate::kdf::HKDF;
-use super::{curve, state::SessionState};
+use crate::state::SessionState;
 
 pub fn initialize_alice_session(
     session_state: &mut SessionState,
