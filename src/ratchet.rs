b0c1eae954c9d6b4cbbf3e07d8d3c5f26cf1e27b
Jack Lloyd <jack@signal.org>
2020-07-10 20:01:04 -0400

    Add logic for session initiation
    Equivalent of SessionBuilder and RatchetSession in Java code

--- src/ratchet.rs
+++ src/ratchet.rs
@@ -4,9 +4,123 @@ mod params;
 pub use self::keys::{ChainKey, MessageKeys, RootKey};
 pub use self::params::{AliceSignalProtocolParameters, BobSignalProtocolParameters};
 use crate::state::SessionState;
+use crate::error::Result;
+use crate::curve;
+use crate::protocol::CIPHERTEXT_MESSAGE_CURRENT_VERSION;
+use crate::proto::storage::SessionStructure;
 
-pub fn initialize_alice_session(
-    session_state: &mut SessionState,
-    parameters: &AliceSignalProtocolParameters,
-) {
+fn derive_keys(secret_input: &[u8]) -> Result<(RootKey, ChainKey)> {
+    let kdf = crate::kdf::HKDF::new(3)?;
+
+    let secrets = kdf.derive_secrets(secret_input, "WhisperText".as_bytes(), 64);
+
+    let root_key = RootKey::new(kdf, &secrets[0..32])?;
+    let chain_key = ChainKey::new(kdf, &secrets[32..64], 0)?;
+
+    Ok((root_key, chain_key))
+}
+
+pub fn initialize_alice_session(parameters: &AliceSignalProtocolParameters) -> Result<SessionState> {
+    let local_identity = parameters.our_identity_key_pair().identity_key();
+
+    let mut csprng = rand::rngs::OsRng; // pass as arg?
+    let sending_ratchet_key = curve::KeyPair::new(&mut csprng);
+
+    let mut secrets = Vec::with_capacity(32 * 5);
+
+    secrets.extend_from_slice(&[0xFFu8; 32]); // "discontinuity bytes"
+
+    let our_base_private_key = parameters.our_base_key_pair().private_key;
+
+    secrets.extend_from_slice(&curve::calculate_agreement(parameters.their_signed_pre_key(),
+                                                          parameters.our_identity_key_pair().private_key())?);
+
+    secrets.extend_from_slice(&curve::calculate_agreement(parameters.their_identity_key().public_key(),
+                                                          &our_base_private_key)?);
+
+    secrets.extend_from_slice(&curve::calculate_agreement(parameters.their_signed_pre_key(),
+                                                          &our_base_private_key)?);
+
+    if let Some(their_one_time_prekey) = parameters.their_one_time_pre_key() {
+        secrets.extend_from_slice(&curve::calculate_agreement(their_one_time_prekey,
+                                                              &our_base_private_key)?);
+    }
+
+    let (root_key, chain_key) = derive_keys(&secrets)?;
+
+    let (sending_chain_root_key, sending_chain_chain_key) =
+        root_key.create_chain(parameters.their_ratchet_key(), &sending_ratchet_key.private_key)?;
+
+    let session = SessionStructure {
+        session_version: CIPHERTEXT_MESSAGE_CURRENT_VERSION as u32,
+        local_identity_public: local_identity.public_key().serialize().to_vec(),
+        remote_identity_public: parameters.their_identity_key().serialize().to_vec(),
+        root_key: sending_chain_root_key.key().to_vec(),
+        previous_counter: 0,
+        sender_chain: None,
+        receiver_chains: vec![],
+        pending_key_exchange: None,
+        pending_pre_key: None,
+        remote_registration_id: 0,
+        local_registration_id: 0,
+        needs_refresh: false,
+        alice_base_key: vec![],
+    };
+
+    let mut session = SessionState::new(session);
+
+    session.add_receiver_chain(parameters.their_ratchet_key(), &chain_key)?;
+    session.set_sender_chain(&sending_ratchet_key, &sending_chain_chain_key)?;
+
+    Ok(session)
+}
+
+pub fn initialize_bob_session(parameters: &BobSignalProtocolParameters) -> Result<SessionState> {
+    let local_identity = parameters.our_identity_key_pair().identity_key();
+
+    let mut secrets = Vec::with_capacity(32 * 5);
+
+    secrets.extend_from_slice(&[0xFFu8; 32]); // "discontinuity bytes"
+
+    secrets.extend_from_slice(&curve::calculate_agreement(parameters.their_identity_key().public_key(),
+                                                          &parameters.our_signed_pre_key_pair().private_key)?);
+
+    secrets.extend_from_slice(&curve::calculate_agreement(parameters.their_base_key(),
+                                                          parameters.our_identity_key_pair().private_key())?);
+
+    secrets.extend_from_slice(&curve::calculate_agreement(parameters.their_base_key(),
+                                                          &parameters.our_signed_pre_key_pair().private_key)?);
+
+    if let Some(our_one_time_pre_key_pair) = parameters.our_one_time_pre_key_pair() {
+        secrets.extend_from_slice(&curve::calculate_agreement(parameters.their_base_key(),
+                                                              &our_one_time_pre_key_pair.private_key)?);
+    }
+
+    let (root_key, chain_key) = derive_keys(&secrets)?;
+
+    let session = SessionStructure {
+        session_version: CIPHERTEXT_MESSAGE_CURRENT_VERSION as u32,
+        local_identity_public: local_identity.public_key().serialize().to_vec(),
+        remote_identity_public: parameters.their_identity_key().serialize().to_vec(),
+        root_key: root_key.key().to_vec(),
+        previous_counter: 0,
+        sender_chain: None,
+        receiver_chains: vec![],
+        pending_key_exchange: None,
+        pending_pre_key: None,
+        remote_registration_id: 0,
+        local_registration_id: 0,
+        needs_refresh: false,
+        alice_base_key: vec![],
+    };
+
+    let mut session = SessionState::new(session);
+
+    session.set_sender_chain(&parameters.our_ratchet_key_pair(), &chain_key)?;
+
+    Ok(session)
+}
+
+pub fn are_we_alice(our_key: &curve::PublicKey, their_key: &curve::PublicKey) -> bool {
+    our_key.serialize() < their_key.serialize()
 }
---
+++ src/session.rs
@@ -0,0 +1,140 @@
+use crate::{IdentityKeyStore,
+            PreKeyStore,
+            SignedPreKeyStore,
+            SessionStore,
+            ProtocolAddress,
+            SessionRecord,
+            SignalProtocolError};
+
+use crate::curve;
+use crate::ratchet;
+use crate::error::Result;
+use crate::protocol::{PreKeySignalMessage};
+use crate::storage::Direction;
+use crate::state::{PreKeyId, PreKeyBundle};
+use crate::ratchet::{AliceSignalProtocolParameters, BobSignalProtocolParameters};
+
+pub struct SessionBuilder {
+    remote_address: ProtocolAddress,
+    session_store: Box<dyn SessionStore>,
+    identity_store: Box<dyn IdentityKeyStore>,
+    signed_prekey_store: Box<dyn SignedPreKeyStore>,
+    pre_key_store: Box<dyn PreKeyStore>,
+}
+
+impl SessionBuilder {
+
+    pub fn new(remote_address: ProtocolAddress,
+               session_store: Box<dyn SessionStore>,
+               identity_store: Box<dyn IdentityKeyStore>,
+               signed_prekey_store: Box<dyn SignedPreKeyStore>,
+               pre_key_store: Box<dyn PreKeyStore>) -> Self {
+        SessionBuilder {
+            remote_address,
+            session_store,
+            identity_store,
+            signed_prekey_store,
+            pre_key_store
+        }
+    }
+
+    pub fn process_prekey(&mut self, session_record: &mut SessionRecord, message: &PreKeySignalMessage) -> Result<Option<PreKeyId>> {
+        let their_identity_key = message.identity_key();
+
+        if self.identity_store.is_trusted_identity(&self.remote_address, their_identity_key, Direction::Receiving)? {
+            return Err(SignalProtocolError::UntrustedIdentity(self.remote_address.clone()));
+        }
+
+        let unsigned_pre_key_id = self.process_prekey_v3(session_record, message)?;
+
+        self.identity_store.save_identity(&self.remote_address, their_identity_key)?;
+
+        Ok(unsigned_pre_key_id)
+    }
+
+    fn process_prekey_v3(&mut self, session_record: &mut SessionRecord, message: &PreKeySignalMessage) -> Result<Option<PreKeyId>> {
+        if session_record.has_session_state(message.message_version() as u32, &message.base_key().serialize())? {
+            // We've already setup a session for this V3 message, letting bundled message fall through
+            return Ok(None);
+        }
+
+        let our_signed_pre_key_pair = self.signed_prekey_store.get_signed_pre_key(message.signed_pre_key_id())?.key_pair()?;
+
+        let our_one_time_pre_key_pair = if let Some(pre_key_id) = message.pre_key_id() {
+            Some(self.pre_key_store.get_pre_key(pre_key_id)?).map(|p| p.key_pair()).transpose()?
+        } else {
+            None
+        };
+
+        let parameters = BobSignalProtocolParameters::new(
+            self.identity_store.get_identity_key_pair()?,
+            our_signed_pre_key_pair, // signed pre key
+            our_one_time_pre_key_pair,
+            our_signed_pre_key_pair, // ratchet key
+            message.identity_key().clone(),
+            *message.base_key());
+
+        session_record.archive_current_state()?;
+
+        let mut new_session = ratchet::initialize_bob_session(&parameters)?;
+
+        new_session.set_local_registration_id(self.identity_store.get_local_registration_id()?)?;
+        new_session.set_remote_registration_id(message.registration_id())?;
+        new_session.set_alice_base_key(&message.base_key().serialize())?;
+
+        session_record.promote_state(new_session)?;
+
+        Ok(message.pre_key_id())
+    }
+
+    fn process_prekey_bundle(&mut self, bundle: &PreKeyBundle) -> Result<()> {
+        let their_identity_key = bundle.identity_key()?;
+
+        if self.identity_store.is_trusted_identity(&self.remote_address, their_identity_key, Direction::Sending)? {
+            return Err(SignalProtocolError::UntrustedIdentity(self.remote_address.clone()));
+        }
+
+        if curve::verify_signature(their_identity_key.public_key(),
+                                   &bundle.signed_pre_key_public()?.serialize(),
+                                   bundle.signed_pre_key_signature()?)? == false {
+            return Err(SignalProtocolError::SignatureValidationFailed);
+        }
+
+        let mut session_record = self.session_store.load_session(&self.remote_address)?.unwrap_or(SessionRecord::new_fresh());
+
+        let mut csprng = rand::rngs::OsRng;
+        let our_base_key_pair = curve::KeyPair::new(&mut csprng);
+        let their_signed_prekey = bundle.signed_pre_key_public()?;
+
+        let their_one_time_prekey = bundle.pre_key_public()?;
+        let their_one_time_prekey_id = bundle.pre_key_id()?;
+
+        let our_identity_key_pair = self.identity_store.get_identity_key_pair()?;
+
+        let parameters = AliceSignalProtocolParameters::new(
+            our_identity_key_pair,
+            our_base_key_pair,
+            *their_identity_key,
+            *their_signed_prekey,
+            *their_one_time_prekey,
+            *their_signed_prekey);
+
+        let mut session = ratchet::initialize_alice_session(&parameters)?;
+
+        session.set_unacknowledged_pre_key_message(their_one_time_prekey_id,
+                                                   bundle.signed_pre_key_id()?,
+                                                   &our_base_key_pair.public_key)?;
+
+        session.set_local_registration_id(self.identity_store.get_local_registration_id()?)?;
+        session.set_remote_registration_id(bundle.registration_id()?)?;
+        session.set_alice_base_key(&our_base_key_pair.public_key.serialize())?;
+
+        self.identity_store.save_identity(&self.remote_address, their_identity_key)?;
+
+        session_record.promote_state(session)?;
+
+        self.session_store.store_session(&self.remote_address, &session_record)?;
+
+        Ok(())
+    }
+}
