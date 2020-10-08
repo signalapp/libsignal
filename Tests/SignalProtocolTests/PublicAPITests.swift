a54dcfebe4792dc52382dac3d85eca30a5b55dd9
Jordan Rose <jrose@signal.org>
2020-10-08 15:48:51 -0700

    Fix order of arguments for hkdf

--- Tests/SignalProtocolTests/PublicAPITests.swift
+++ Tests/SignalProtocolTests/PublicAPITests.swift
@@ -2,8 +2,7 @@ import XCTest
 import SignalProtocol
 
 class PublicAPITests: XCTestCase {
-    func testHkdf() {
-
+    func testHkdfSimple() {
         let ikm: [UInt8] = [
             0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b,
             0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b,
@@ -23,8 +22,29 @@ class PublicAPITests: XCTestCase {
         XCTAssertThrowsError(try hkdf(outputLength: okm.count,
                                       version: 19,
                                       inputKeyMaterial: ikm,
-                                  salt: salt,
-                                  info: info))
+                                      salt: salt,
+                                      info: info))
+    }
+
+    func testHkdfUsingRFCExample() {
+        // https://tools.ietf.org/html/rfc5869 A.2
+        let ikm: [UInt8] = Array(0...0x4f)
+        let salt: [UInt8] = Array(0x60...0xaf)
+        let info: [UInt8] = Array(0xb0...0xff)
+        let okm: [UInt8] = [0xb1, 0x1e, 0x39, 0x8d, 0xc8, 0x03, 0x27, 0xa1, 0xc8, 0xe7, 0xf7, 0x8c, 0x59, 0x6a, 0x49, 0x34,
+                            0x4f, 0x01, 0x2e, 0xda, 0x2d, 0x4e, 0xfa, 0xd8, 0xa0, 0x50, 0xcc, 0x4c, 0x19, 0xaf, 0xa9, 0x7c,
+                            0x59, 0x04, 0x5a, 0x99, 0xca, 0xc7, 0x82, 0x72, 0x71, 0xcb, 0x41, 0xc6, 0x5e, 0x59, 0x0e, 0x09,
+                            0xda, 0x32, 0x75, 0x60, 0x0c, 0x2f, 0x09, 0xb8, 0x36, 0x77, 0x93, 0xa9, 0xac, 0xa3, 0xdb, 0x71,
+                            0xcc, 0x30, 0xc5, 0x81, 0x79, 0xec, 0x3e, 0x87, 0xc1, 0x4c, 0x01, 0xd5, 0xc1, 0xf3, 0x43, 0x4f,
+                            0x1d, 0x87]
+
+        let version = UInt32(3)
+        let derived = try! hkdf(outputLength: okm.count,
+                                version: version,
+                                inputKeyMaterial: ikm,
+                                salt: salt,
+                                info: info)
+        XCTAssertEqual(derived, okm)
     }
 
     func testAddress() {
@@ -297,12 +317,13 @@ class PublicAPITests: XCTestCase {
 
     static var allTests: [(String, (PublicAPITests) -> () throws -> Void)] {
         return [
-          ("testAddreses", testAddress),
-          ("testFingerprint", testFingerprint),
-          ("testPkOperations", testPkOperations),
-          ("testHkdf", testHkdf),
-          ("testGroupCipher", testGroupCipher),
-          ("testSessionCipher", testSessionCipher),
+            ("testAddreses", testAddress),
+            ("testFingerprint", testFingerprint),
+            ("testPkOperations", testPkOperations),
+            ("testHkdfSimple", testHkdfSimple),
+            ("testHkdfUsingRFCExample", testHkdfUsingRFCExample),
+            ("testGroupCipher", testGroupCipher),
+            ("testSessionCipher", testSessionCipher),
         ]
     }
 }
