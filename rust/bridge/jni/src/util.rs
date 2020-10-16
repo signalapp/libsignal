c1331eb42f71184996e3994b52023a40ec47c0dd
Jack Lloyd <jack@signal.org>
2020-10-16 18:30:17 -0400

    Add license headers

--- rust/bridge/jni/src/util.rs
+++ rust/bridge/jni/src/util.rs
@@ -1,3 +1,10 @@
+//
+// Copyright (C) 2020 Signal Messenger, LLC.
+// All rights reserved.
+//
+// SPDX-License-Identifier: GPL-3.0-only
+//
+
 use jni::objects::{JObject, JString, JThrowable, JValue};
 use jni::sys::{_jobject, jboolean, jbyteArray, jint, jlong, jobject, jstring};
 use jni::JNIEnv;
