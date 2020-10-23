be56945a70d1bb5e0371ebdc78854d533b34bc70
Jordan Rose <jrose@signal.org>
2020-10-23 11:44:41 -0700

    Add a Yarn-based Node package for signal-client
    There's very little here yet, just enough to test something.

---
+++ node/.eslintrc.js
@@ -0,0 +1,64 @@
+// For reference: https://github.com/airbnb/javascript
+
+module.exports = {
+  settings: {
+    'import/core-modules': ['electron'],
+  },
+
+  extends: ['airbnb-base', 'prettier'],
+
+  plugins: ['mocha', 'more'],
+
+  rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+
+    // prevents us from accidentally checking in exclusive tests (`.only`):
+    'mocha/no-exclusive-tests': 'error',
+
+    // encourage consistent use of `async` / `await` instead of `then`
+    'more/no-then': 'error',
+
+    // it helps readability to put public API at top,
+    'no-use-before-define': 'off',
+
+    // useful for unused or internal fields
+    'no-underscore-dangle': 'off',
+
+    // though we have a logger, we still remap console to log to disk
+    'no-console': 'error',
+
+    // consistently place operators at end of line except ternaries
+    'operator-linebreak': 'error',
+
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: false },
+    ],
+
+    // Prettier overrides:
+    'arrow-parens': 'off',
+    'function-paren-newline': 'off',
+    'max-len': [
+      'error',
+      {
+        // Prettier generally limits line length to 80 but sometimes goes over.
+        // The `max-len` plugin doesn’t let us omit `code` so we set it to a
+        // high value as a buffer to let Prettier control the line length:
+        code: 999,
+        // We still want to limit comments as before:
+        comments: 90,
+        ignoreUrls: true,
+      },
+    ],
+  },
+};
---
+++ node/libsignal_client.d.ts
@@ -0,0 +1,9 @@
+//
+// Copyright 2020 Signal Messenger, LLC.
+// SPDX-License-Identifier: AGPL-3.0-only
+//
+
+export class PrivateKey {
+  constructor();
+  serialize(): Buffer;
+}
