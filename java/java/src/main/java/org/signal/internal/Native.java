df87cd2edf7a7d61db0f1e3ad6cbfbe92378bc40
Jordan Rose <jrose@signal.org>
2020-12-01 16:50:49 -0800

    UnidentifiedSenderMessageContent: Don't validate the sender up front
    Both the Android and iOS clients currently allow customizing this
    validatation logic for testing purposes, and iOS additionally wants to
    log the information about the unwrapped message even if there are
    problems with it.
    
    iOS does also currently log information about the unwrapped message
    even if the static key being used is wrong, but in order to fix that
    we'd have to have a secondary return value (either the expected static
    key, computed during decrypt-to-USMC, or a flag saying whether the
    comparison failed).

--- java/java/src/main/java/org/signal/internal/Native.java
+++ java/java/src/main/java/org/signal/internal/Native.java
@@ -136,7 +136,7 @@ public final class Native {
 
   public static native boolean ScannableFingerprint_Compare(byte[] fprint1, byte[] fprint2);
 
-  public static native long SealedSessionCipher_DecryptToUsmc(byte[] ctext, long trustRoot, long timestamp, IdentityKeyStore identityStore);
+  public static native long SealedSessionCipher_DecryptToUsmc(byte[] ctext, IdentityKeyStore identityStore);
   public static native byte[] SealedSessionCipher_Encrypt(long destination, long senderCert, byte[] ptext, SessionStore sessionStore, IdentityKeyStore identityStore);
 
   public static native long SenderCertificate_Deserialize(byte[] data);
