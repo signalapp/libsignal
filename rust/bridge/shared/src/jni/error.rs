10a117190a36bee7189acee4b3a3fea7fa791079
Jordan Rose <jrose@signal.org>
2020-12-07 12:30:06 -0800

    JNI: remove unused helper function

--- rust/bridge/shared/src/jni/error.rs
+++ rust/bridge/shared/src/jni/error.rs
@@ -28,16 +28,46 @@ pub enum SignalJniError {
     ExceptionDuringCallback(String),
 }
 
-impl SignalJniError {
-    pub fn to_signal_protocol_error(&self) -> SignalProtocolError {
+impl fmt::Display for SignalJniError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            SignalJniError::Signal(e) => e.clone(),
-            SignalJniError::Jni(e) => SignalProtocolError::FfiBindingError(e.to_string()),
-            SignalJniError::BadJniParameter(m) => {
-                SignalProtocolError::InvalidArgument(m.to_string())
+            SignalJniError::Signal(s) => write!(f, "{}", s),
+            SignalJniError::AesGcmSiv(s) => write!(f, "{}", s),
+            SignalJniError::Jni(s) => write!(f, "JNI error {}", s),
+            SignalJniError::ExceptionDuringCallback(s) => {
+                write!(f, "exception recieved during callback {}", s)
             }
-            _ => SignalProtocolError::FfiBindingError(format!("{}", self)),
+            SignalJniError::NullHandle => write!(f, "null handle"),
+            SignalJniError::BadJniParameter(m) => write!(f, "bad parameter type {}", m),
+            SignalJniError::UnexpectedJniResultType(m, t) => {
+                write!(f, "calling {} returned unexpected type {}", m, t)
+            }
+            SignalJniError::IntegerOverflow(m) => {
+                write!(f, "integer overflow during conversion of {}", m)
+            }
+            SignalJniError::UnexpectedPanic(e) => match e.downcast_ref::<&'static str>() {
+                Some(s) => write!(f, "unexpected panic: {}", s),
+                None => write!(f, "unknown unexpected panic"),
+            },
         }
+    }
+}
+
+impl From<SignalProtocolError> for SignalJniError {
+    fn from(e: SignalProtocolError) -> SignalJniError {
+        SignalJniError::Signal(e)
+    }
+}
+
+impl From<AesGcmSivError> for SignalJniError {
+    fn from(e: AesGcmSivError) -> SignalJniError {
+        SignalJniError::AesGcmSiv(e)
+    }
+}
+
+impl From<jni::errors::Error> for SignalJniError {
+    fn from(e: jni::errors::Error) -> SignalJniError {
+        SignalJniError::Jni(e)
     }
 }
 
