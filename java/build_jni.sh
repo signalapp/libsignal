d05089e6e8b75eeae03a074d7f177147cca60c8c
Jordan Rose <jrose@signal.org>
2020-12-08 14:40:30 -0800

    Java: get local tests working on Mac as well

--- java/build_jni.sh
+++ java/build_jni.sh
@@ -5,6 +5,12 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 #
 
+set -euo pipefail
+
+SCRIPT_DIR=$(dirname "$0")
+cd "${SCRIPT_DIR}"/..
+. bin/build_helpers.sh
+
 # These paths are relative to the root directory
 ANDROID_LIB_DIR=java/android/src/main/jniLibs
 DESKTOP_LIB_DIR=java/java/src/main/resources
@@ -12,17 +18,16 @@ DESKTOP_LIB_DIR=java/java/src/main/resources
 export RUSTFLAGS="-C link-args=-s"
 export CARGO_PROFILE_RELEASE_DEBUG=1 # enable line tables
 
-cd .. || exit
-
 if [ "$1" = 'desktop' ];
 then
-   cargo build -Z unstable-options -p libsignal-jni --release --target x86_64-unknown-linux-gnu --out-dir=$DESKTOP_LIB_DIR
+    echo_then_run cargo build -p libsignal-jni --release
+    copy_built_library target/release signal_jni $DESKTOP_LIB_DIR/
 elif [ "$1" = 'android' ];
 then
-    cargo ndk --target armv7-linux-androideabi --platform 19 -- build -Z unstable-options -p libsignal-jni --release --out-dir=$ANDROID_LIB_DIR/armeabi-v7a
-    cargo ndk --target aarch64-linux-android --platform 21 -- build -Z unstable-options -p libsignal-jni --release --out-dir=$ANDROID_LIB_DIR/arm64-v8a
-    cargo ndk --target i686-linux-android --platform 19 -- build -Z unstable-options -p libsignal-jni --release --out-dir=$ANDROID_LIB_DIR/x86
-    cargo ndk --target x86_64-linux-android --platform 21 -- build -Z unstable-options -p libsignal-jni --release --out-dir=$ANDROID_LIB_DIR/x86_64
+    echo_then_run cargo ndk --target armv7-linux-androideabi --platform 19 -- build -Z unstable-options -p libsignal-jni --release --out-dir=$ANDROID_LIB_DIR/armeabi-v7a
+    echo_then_run cargo ndk --target aarch64-linux-android --platform 21 -- build -Z unstable-options -p libsignal-jni --release --out-dir=$ANDROID_LIB_DIR/arm64-v8a
+    echo_then_run cargo ndk --target i686-linux-android --platform 19 -- build -Z unstable-options -p libsignal-jni --release --out-dir=$ANDROID_LIB_DIR/x86
+    echo_then_run cargo ndk --target x86_64-linux-android --platform 21 -- build -Z unstable-options -p libsignal-jni --release --out-dir=$ANDROID_LIB_DIR/x86_64
 else
     echo "Unknown target (use 'desktop' or 'android')"
 fi
