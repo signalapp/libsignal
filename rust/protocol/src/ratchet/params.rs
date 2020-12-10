9ac691239770fb64d6b337fd9dc57919850aed50
Jack Lloyd <jack@signal.org>
2020-12-10 08:49:44 -0500

    Clean up some imports
    Why was this re-exporting the type with a new name?

--- rust/protocol/src/ratchet/params.rs
+++ rust/protocol/src/ratchet/params.rs
@@ -3,27 +3,26 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 //
 
-pub use super::super::curve::{KeyPair as CurveKeyPair, PublicKey as CurvePublicKey};
-pub use super::super::{IdentityKey, IdentityKeyPair};
+use crate::{IdentityKey, IdentityKeyPair, KeyPair, PublicKey};
 
 pub struct AliceSignalProtocolParameters {
     our_identity_key_pair: IdentityKeyPair,
-    our_base_key_pair: CurveKeyPair,
+    our_base_key_pair: KeyPair,
 
     their_identity_key: IdentityKey,
-    their_signed_pre_key: CurvePublicKey,
-    their_one_time_pre_key: Option<CurvePublicKey>,
-    their_ratchet_key: CurvePublicKey,
+    their_signed_pre_key: PublicKey,
+    their_one_time_pre_key: Option<PublicKey>,
+    their_ratchet_key: PublicKey,
 }
 
 impl AliceSignalProtocolParameters {
     pub fn new(
         our_identity_key_pair: IdentityKeyPair,
-        our_base_key_pair: CurveKeyPair,
+        our_base_key_pair: KeyPair,
         their_identity_key: IdentityKey,
-        their_signed_pre_key: CurvePublicKey,
-        their_one_time_pre_key: Option<CurvePublicKey>,
-        their_ratchet_key: CurvePublicKey,
+        their_signed_pre_key: PublicKey,
+        their_one_time_pre_key: Option<PublicKey>,
+        their_ratchet_key: PublicKey,
     ) -> Self {
         Self {
             our_identity_key_pair,
@@ -41,7 +40,7 @@ impl AliceSignalProtocolParameters {
     }
 
     #[inline]
-    pub fn our_base_key_pair(&self) -> &CurveKeyPair {
+    pub fn our_base_key_pair(&self) -> &KeyPair {
         &self.our_base_key_pair
     }
 
@@ -51,39 +50,39 @@ impl AliceSignalProtocolParameters {
     }
 
     #[inline]
-    pub fn their_signed_pre_key(&self) -> &CurvePublicKey {
+    pub fn their_signed_pre_key(&self) -> &PublicKey {
         &self.their_signed_pre_key
     }
 
     #[inline]
-    pub fn their_one_time_pre_key(&self) -> Option<&CurvePublicKey> {
+    pub fn their_one_time_pre_key(&self) -> Option<&PublicKey> {
         self.their_one_time_pre_key.as_ref()
     }
 
     #[inline]
-    pub fn their_ratchet_key(&self) -> &CurvePublicKey {
+    pub fn their_ratchet_key(&self) -> &PublicKey {
         &self.their_ratchet_key
     }
 }
 
 pub struct BobSignalProtocolParameters {
     our_identity_key_pair: IdentityKeyPair,
-    our_signed_pre_key_pair: CurveKeyPair,
-    our_one_time_pre_key_pair: Option<CurveKeyPair>,
-    our_ratchet_key_pair: CurveKeyPair,
+    our_signed_pre_key_pair: KeyPair,
+    our_one_time_pre_key_pair: Option<KeyPair>,
+    our_ratchet_key_pair: KeyPair,
 
     their_identity_key: IdentityKey,
-    their_base_key: CurvePublicKey,
+    their_base_key: PublicKey,
 }
 
 impl BobSignalProtocolParameters {
     pub fn new(
         our_identity_key_pair: IdentityKeyPair,
-        our_signed_pre_key_pair: CurveKeyPair,
-        our_one_time_pre_key_pair: Option<CurveKeyPair>,
-        our_ratchet_key_pair: CurveKeyPair,
+        our_signed_pre_key_pair: KeyPair,
+        our_one_time_pre_key_pair: Option<KeyPair>,
+        our_ratchet_key_pair: KeyPair,
         their_identity_key: IdentityKey,
-        their_base_key: CurvePublicKey,
+        their_base_key: PublicKey,
     ) -> Self {
         Self {
             our_identity_key_pair,
@@ -101,17 +100,17 @@ impl BobSignalProtocolParameters {
     }
 
     #[inline]
-    pub fn our_signed_pre_key_pair(&self) -> &CurveKeyPair {
+    pub fn our_signed_pre_key_pair(&self) -> &KeyPair {
         &self.our_signed_pre_key_pair
     }
 
     #[inline]
-    pub fn our_one_time_pre_key_pair(&self) -> Option<&CurveKeyPair> {
+    pub fn our_one_time_pre_key_pair(&self) -> Option<&KeyPair> {
         self.our_one_time_pre_key_pair.as_ref()
     }
 
     #[inline]
-    pub fn our_ratchet_key_pair(&self) -> &CurveKeyPair {
+    pub fn our_ratchet_key_pair(&self) -> &KeyPair {
         &self.our_ratchet_key_pair
     }
 
@@ -121,7 +120,7 @@ impl BobSignalProtocolParameters {
     }
 
     #[inline]
-    pub fn their_base_key(&self) -> &CurvePublicKey {
+    pub fn their_base_key(&self) -> &PublicKey {
         &self.their_base_key
     }
 }
