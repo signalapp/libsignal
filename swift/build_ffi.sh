baba4519debde85e26f85469af6980977191bbc6
Jordan Rose <jrose@signal.org>
2021-01-04 12:19:09 -0800

    Swift: Generate signal_ffi.h offline, like Native.java
    - Verify its correctness with build_ffi.sh --verify-ffi
    - Regenerate with build_ffi.sh --generate-ffi
    
    This simplifies the header search logic for both SwiftPM and
    CocoaPods, as well as saving on build time by avoiding cbindgen.
    
    The tweak to cbindgen.toml to prefer typedef-based structs and enums
    is sidestepping an incompatibility between cbindgen 0.15.0 (which
    GitHub has installed) and 0.16.0 (which allows reusing a release build
    directory as well as a debug one).

--- swift/build_ffi.sh
+++ swift/build_ffi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright 2020 Signal Messenger, LLC.
+# Copyright 2020-2021 Signal Messenger, LLC.
 # SPDX-License-Identifier: AGPL-3.0-only
 #
 
@@ -17,17 +17,46 @@ Usage: $(basename "$0") [-d]
 
 Options:
 	-d -- debug build (default is release)
+	-v -- verbose build
+
+	--generate-ffi -- regenerate ffi headers
+	--verify-ffi   -- verify that ffi headers are up to date
 
 Use CARGO_BUILD_TARGET for cross-compilation (such as for iOS).
 END
 }
 
+check_cbindgen() {
+  if ! which cbindgen > /dev/null; then
+    echo 'error: cbindgen not found in PATH' >&2
+    if which cargo > /dev/null; then
+      echo 'note: get it by running' >&2
+      printf "\n\t%s\n\n" "cargo install cbindgen --vers '^0.16'" >&2
+    fi
+    exit 1
+  fi
+}
+
+
 RELEASE_BUILD=1
+VERBOSE=
+SHOULD_CBINDGEN=
+CBINDGEN_VERIFY=
 
 while [ "${1:-}" != "" ]; do
   case $1 in
     -d | --debug )
       RELEASE_BUILD=
+      ;;
+    -v | --verbose )
+      VERBOSE=1
+      ;;
+    --generate-ffi )
+      SHOULD_CBINDGEN=1
+      ;;
+    --verify-ffi )
+      SHOULD_CBINDGEN=1
+      CBINDGEN_VERIFY=1
       ;;
     -h | --help )
       usage
@@ -35,7 +64,7 @@ while [ "${1:-}" != "" ]; do
       ;;
     * )
       usage
-      exit 1
+      exit 2
   esac
   shift
 done
@@ -50,5 +79,25 @@ if [[ -n "${DEVELOPER_SDK_DIR:-}" ]]; then
   export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/MacOSX.sdk/usr/lib:${LIBRARY_PATH:-}"
 fi
 
-set -x
-cargo build -p libsignal-ffi ${RELEASE_BUILD:+--release}
+echo_then_run cargo build -p libsignal-ffi ${RELEASE_BUILD:+--release} ${VERBOSE:+--verbose}
+
+FFI_HEADER_PATH=swift/Sources/SignalFfi/signal_ffi.h
+
+if [[ -n "${SHOULD_CBINDGEN}" ]]; then
+  check_cbindgen
+  if [[ -n "${CBINDGEN_VERIFY}" ]]; then
+    echo diff -u "${FFI_HEADER_PATH}" "<(cbindgen -q ${RELEASE_BUILD:+--profile release} rust/bridge/ffi)"
+    if ! diff -u "${FFI_HEADER_PATH}"  <(cbindgen -q ${RELEASE_BUILD:+--profile release} rust/bridge/ffi); then
+      echo
+      echo 'error: signal_ffi.h not up to date; run' "$0" '--generate-ffi' >&2
+      exit 1
+    fi
+  else
+    echo cbindgen ${RELEASE_BUILD:+--profile release} -o "${FFI_HEADER_PATH}" rust/bridge/ffi
+    # Use sed to ignore irrelevant cbindgen warnings.
+    # ...and then disable the shellcheck warning about literal backticks in single-quotes
+    # shellcheck disable=SC2016
+    cbindgen ${RELEASE_BUILD:+--profile release} -o "${FFI_HEADER_PATH}" rust/bridge/ffi 2>&1 |
+      sed '/WARN: Missing `\[defines\]` entry for `feature = "ffi"` in cbindgen config\./ d' >&2
+  fi
+fi
