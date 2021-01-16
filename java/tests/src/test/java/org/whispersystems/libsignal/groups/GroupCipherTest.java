5e76de2559888f1bfaa2ccd835aa7b8fefd92672
Jack Lloyd <jack@signal.org>
2021-01-16 15:07:42 -0500

    Drop down to 25k

--- java/tests/src/test/java/org/whispersystems/libsignal/groups/GroupCipherTest.java
+++ java/tests/src/test/java/org/whispersystems/libsignal/groups/GroupCipherTest.java
@@ -229,7 +229,7 @@ public class GroupCipherTest extends TestCase {
 
     bobSessionBuilder.process(aliceName, aliceDistributionMessage);
 
-    for (int i=0;i<1000001;i++) {
+    for (int i=0;i<25001;i++) {
       aliceGroupCipher.encrypt("up the punks".getBytes());
     }
 
