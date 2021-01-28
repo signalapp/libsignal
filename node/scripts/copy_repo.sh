abfc1bff3f41ad3a6b564cd901c0bc89361345b6
Jack Lloyd <jack@signal.org>
2021-01-28 18:19:44 -0500

    Tweak copy_repo script

--- node/scripts/copy_repo.sh
+++ node/scripts/copy_repo.sh
@@ -8,8 +8,8 @@
 set -euo pipefail
 
 SCRIPT_DIR=$(dirname "$0")
-cd "${SCRIPT_DIR}"/..
-. ../bin/build_helpers.sh
+cd "${SCRIPT_DIR}"/../..
+. bin/build_helpers.sh
 
 #
 # copy_repo.sh <source> <destination>
@@ -17,28 +17,25 @@ cd "${SCRIPT_DIR}"/..
 # Copy the given node directory to the artifact repository.
 #
 # Example:
-# libsignal-client/node$ scripts/copy_repo.sh . ../../libsignal-client-node
+# libsignal-client$ node/scripts/copy_repo.sh ../libsignal-client-node
 #
 
-rsync -avrv \
-  --exclude='dist/test' \
-  --exclude='node_modules' \
-  --exclude='scripts' \
-  --exclude='test' \
-  --exclude='.gitignore' \
-  --exclude='.nvmrc' \
-  --exclude='Makefile' \
-  --exclude='tsconfig.json' \
-  --exclude='BUILDING.md' \
-  --exclude='README.md' \
-  --exclude='build_node_bridge.py' \
-   $1 $2
+mkdir -p $1
 
-mkdir -p $2/build
-cp -vf ../build/Release/libsignal_client_*.node $2/build
+cp -vf package.json $1
+cp -vf node/index.ts $1
+cp -vf node/libsignal_client.d.ts $1
+
+mkdir -p $1/dist
+cp -vf node/dist/index.d.ts $1/dist
+cp -vf node/dist/index.js $1/dist
+cp -vf node/libsignal_client.d.ts $1/dist
+
+mkdir -p $1/build
+cp -vf build/Release/libsignal_client_*.node $1/build
 
 # Ensure that the LICENSE file is up to date.
-cp -vf $1/../LICENSE $2
+cp -vf ./LICENSE $1
 
 # Ensure that the README.md file is up to date.
-cp -vf $1/../README.md $2
+cp -vf ./README.md $1
