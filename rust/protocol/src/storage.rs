0ea9185343db1a942fe784fe75084733a48de71c
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2021-06-07 20:43:48 -0700

    [#468] add docs to storage.rs
    - [+INMEM/TRAITS] documentation for the store interfaces and impls

--- rust/protocol/src/storage.rs
+++ rust/protocol/src/storage.rs
@@ -1,7 +1,11 @@
 //
-// Copyright 2020 Signal Messenger, LLC.
+// Copyright 2020-2022 Signal Messenger, LLC.
 // SPDX-License-Identifier: AGPL-3.0-only
 //
+
+//! Interfaces in [traits] and reference implementations in [inmem] for various mutable stores.
+
+#![warn(missing_docs)]
 
 mod inmem;
 mod traits;
