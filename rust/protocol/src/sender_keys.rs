aaa24f6efae6114f1252ae86f02aeedc69a8b151
Alan Evans <alan@signal.org>
2021-09-03 14:03:25 -0300

    Handle repeated (public_key, chain_id) pairs

--- rust/protocol/src/sender_keys.rs
+++ rust/protocol/src/sender_keys.rs
@@ -3,15 +3,17 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 //
 
+use std::collections::VecDeque;
+use std::convert::TryFrom;
+
+use itertools::Itertools;
+use prost::Message;
+use uuid::Uuid;
+
 use crate::consts;
 use crate::crypto::hmac_sha256;
 use crate::proto::storage as storage_proto;
 use crate::{PrivateKey, PublicKey, Result, SignalProtocolError, HKDF};
-
-use prost::Message;
-use std::collections::VecDeque;
-use std::convert::TryFrom;
-use uuid::Uuid;
 
 #[derive(Debug, Clone)]
 pub struct SenderMessageKey {
@@ -254,7 +256,7 @@ pub struct SenderKeyRecord {
 impl SenderKeyRecord {
     pub fn new_empty() -> Self {
         Self {
-            states: VecDeque::new(),
+            states: VecDeque::with_capacity(consts::MAX_SENDER_KEY_STATES),
         }
     }
 
@@ -310,19 +312,56 @@ impl SenderKeyRecord {
         signature_key: PublicKey,
         signature_private_key: Option<PrivateKey>,
     ) -> Result<()> {
-        self.states.push_front(SenderKeyState::new(
-            message_version,
-            chain_id,
-            iteration,
-            chain_key,
-            signature_key,
-            signature_private_key,
-        )?);
+        let existing_state = self.remove_state(chain_id, signature_key);
 
-        while self.states.len() > consts::MAX_SENDER_KEY_STATES {
+        if self.remove_states_with_chain_id(chain_id) > 0 {
+            log::warn!(
+                "Removed a matching chain_id ({}) found with a different public key",
+                chain_id
+            );
+        }
+
+        let state = match existing_state {
+            None => SenderKeyState::new(
+                message_version,
+                chain_id,
+                iteration,
+                chain_key,
+                signature_key,
+                signature_private_key,
+            )?,
+            Some(state) => state,
+        };
+
+        while self.states.len() >= consts::MAX_SENDER_KEY_STATES {
             self.states.pop_back();
         }
+
+        self.states.push_front(state);
+
         Ok(())
+    }
+
+    /// Remove the state with the matching `chain_id` and `signature_key`.
+    ///
+    /// Skips any bad protobufs.
+    fn remove_state(&mut self, chain_id: u32, signature_key: PublicKey) -> Option<SenderKeyState> {
+        let (index, _state) = self.states.iter().find_position(|state| {
+            state.chain_id().ok() == Some(chain_id)
+                && state.signing_key_public().ok() == Some(signature_key)
+        })?;
+
+        self.states.remove(index)
+    }
+
+    /// Returns the number of removed states.
+    ///
+    /// Skips any bad protobufs.
+    fn remove_states_with_chain_id(&mut self, chain_id: u32) -> usize {
+        let initial_length = self.states.len();
+        self.states
+            .retain(|state| state.chain_id().ok() != Some(chain_id));
+        initial_length - self.states.len()
     }
 
     pub fn set_sender_key_state(
@@ -358,5 +397,233 @@ impl SenderKeyRecord {
 
     pub fn serialize(&self) -> Result<Vec<u8>> {
         Ok(self.as_protobuf()?.encode_to_vec())
+    }
+}
+
+#[cfg(test)]
+mod sender_key_record_add_sender_key_state_tests {
+    use itertools::Itertools;
+    use rand::rngs::OsRng;
+
+    use crate::KeyPair;
+
+    use super::*;
+
+    fn random_public_key() -> PublicKey {
+        KeyPair::generate(&mut OsRng).public_key
+    }
+
+    fn chain_key(i: u128) -> Vec<u8> {
+        i.to_be_bytes().to_vec()
+    }
+
+    struct TestContext {
+        sender_key_record: SenderKeyRecord,
+    }
+
+    impl TestContext {
+        fn new() -> Self {
+            Self {
+                sender_key_record: SenderKeyRecord::new_empty(),
+            }
+        }
+
+        /// Associates the `record_key` with the `chain_key` via `add_sender_key_state` which is the
+        /// method under test in this module.
+        fn add_sender_key_state_record(&mut self, record_key: (PublicKey, u32), chain_key: &[u8]) {
+            let (public_key, chain_id) = record_key;
+            self.sender_key_record
+                .add_sender_key_state(1, chain_id, 1, chain_key, public_key, None)
+                .expect("to be able to add new state");
+        }
+
+        fn assert_number_of_states(&self, expected: usize) {
+            assert_eq!(expected, self.sender_key_record.states.len());
+        }
+
+        /// Asserts that for the supplied `record_key` the chain key is as expected when looked up
+        /// by both `chain_id` and `public_key` and `chain_id`.
+        fn assert_records_chain_key(
+            &mut self,
+            record_key: (PublicKey, u32),
+            expected_chain_key: &[u8],
+        ) {
+            let (public_key, chain_id) = record_key;
+            let found_chain_key = self
+                .sender_key_record
+                .sender_key_state_for_chain_id(chain_id, Uuid::default())
+                .expect("Expect to find chain id")
+                .sender_chain_key()
+                .expect("Expect to find chain key")
+                .chain_key;
+
+            assert_eq!(found_chain_key, expected_chain_key);
+
+            let matching_state = self
+                .sender_key_record
+                .states
+                .iter()
+                .filter(|state| {
+                    state.chain_id().expect("expect chain id") == chain_id
+                        && state.signing_key_public().expect("expect public key") == public_key
+                })
+                .exactly_one()
+                .expect("Expected exactly one record key match");
+
+            assert_eq!(
+                &matching_state
+                    .sender_chain_key()
+                    .expect("Expect to find chain key")
+                    .chain_key,
+                expected_chain_key
+            );
+        }
+
+        fn assert_record_order(&self, order: Vec<(PublicKey, u32)>) {
+            let record_keys = self
+                .sender_key_record
+                .states
+                .iter()
+                .map(|state| {
+                    (
+                        state.signing_key_public().expect("expect public key"),
+                        state.chain_id().expect("expect chain id"),
+                    )
+                })
+                .collect::<Vec<_>>();
+
+            assert_eq!(record_keys, order);
+        }
+    }
+
+    #[test]
+    fn add_single_state() {
+        let mut context = TestContext::new();
+
+        let public_key = random_public_key();
+        let chain_id = 1;
+        let chain_key = chain_key(1);
+        let record_key = (public_key, chain_id);
+
+        context.add_sender_key_state_record(record_key, &chain_key);
+
+        context.assert_number_of_states(1);
+        context.assert_records_chain_key(record_key, &chain_key);
+    }
+
+    #[test]
+    fn add_second_state() {
+        let mut context = TestContext::new();
+
+        let chain_id_1 = 1;
+        let chain_id_2 = 2;
+        let record_key_1 = (random_public_key(), chain_id_1);
+        let record_key_2 = (random_public_key(), chain_id_2);
+        let chain_key_1 = chain_key(1);
+        let chain_key_2 = chain_key(2);
+
+        context.add_sender_key_state_record(record_key_1, &chain_key_1);
+        context.add_sender_key_state_record(record_key_2, &chain_key_2);
+
+        context.assert_number_of_states(2);
+        context.assert_records_chain_key(record_key_1, &chain_key_1);
+        context.assert_records_chain_key(record_key_2, &chain_key_2);
+    }
+
+    #[test]
+    fn when_exceed_maximum_states_then_oldest_is_ejected() {
+        assert_eq!(
+            5,
+            consts::MAX_SENDER_KEY_STATES,
+            "Test written to expect this limit"
+        );
+
+        let mut context = TestContext::new();
+
+        let record_key_1 = (random_public_key(), 1);
+        let record_key_2 = (random_public_key(), 2);
+        let record_key_3 = (random_public_key(), 3);
+        let record_key_4 = (random_public_key(), 4);
+        let record_key_5 = (random_public_key(), 5);
+        let record_key_6 = (random_public_key(), 6);
+
+        context.add_sender_key_state_record(record_key_1, &chain_key(1));
+        context.add_sender_key_state_record(record_key_2, &chain_key(2));
+        context.add_sender_key_state_record(record_key_3, &chain_key(3));
+        context.add_sender_key_state_record(record_key_4, &chain_key(4));
+        context.add_sender_key_state_record(record_key_5, &chain_key(5));
+
+        context.assert_record_order(vec![
+            record_key_5,
+            record_key_4,
+            record_key_3,
+            record_key_2,
+            record_key_1,
+        ]);
+
+        context.add_sender_key_state_record(record_key_6, &chain_key(6));
+
+        context.assert_record_order(vec![
+            record_key_6,
+            record_key_5,
+            record_key_4,
+            record_key_3,
+            record_key_2,
+        ]);
+    }
+
+    #[test]
+    fn when_second_state_with_same_public_key_and_chain_id_added_then_it_keeps_first_data() {
+        let mut context = TestContext::new();
+
+        let chain_id = 1;
+        let record_key = (random_public_key(), chain_id);
+        let chain_key_1 = chain_key(1);
+        let chain_key_2 = chain_key(2);
+
+        context.add_sender_key_state_record(record_key, &chain_key_1);
+        context.add_sender_key_state_record(record_key, &chain_key_2);
+
+        context.assert_number_of_states(1);
+        context.assert_records_chain_key(record_key, &chain_key_1);
+    }
+
+    #[test]
+    fn when_second_state_with_different_public_key_but_same_chain_id_added_then_it_gets_replaced() {
+        let mut context = TestContext::new();
+
+        let chain_id = 1;
+        let record_key_1 = (random_public_key(), chain_id);
+        let record_key_2 = (random_public_key(), chain_id);
+        let chain_key_1 = chain_key(1);
+        let chain_key_2 = chain_key(2);
+
+        context.add_sender_key_state_record(record_key_1, &chain_key_1);
+        context.add_sender_key_state_record(record_key_2, &chain_key_2);
+
+        context.assert_number_of_states(1);
+        context.assert_records_chain_key(record_key_2, &chain_key_2);
+    }
+
+    #[test]
+    fn when_second_state_with_same_public_key_and_chain_id_added_then_it_becomes_the_most_recent() {
+        let mut context = TestContext::new();
+
+        let chain_id_1 = 1;
+        let chain_id_2 = 2;
+        let record_key_1 = (random_public_key(), chain_id_1);
+        let record_key_2 = (random_public_key(), chain_id_2);
+        let chain_key_1 = chain_key(1);
+        let chain_key_2 = chain_key(2);
+        let chain_key_3 = chain_key(3);
+
+        context.add_sender_key_state_record(record_key_1, &chain_key_1);
+        context.add_sender_key_state_record(record_key_2, &chain_key_2);
+
+        context.assert_record_order(vec![record_key_2, record_key_1]);
+
+        context.add_sender_key_state_record(record_key_1, &chain_key_3);
+
+        context.assert_record_order(vec![record_key_1, record_key_2]);
     }
 }
