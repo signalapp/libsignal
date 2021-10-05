1d99ef058088d89955da64de989759463074ad2f
Jordan Rose <jrose@signal.org>
2021-10-04 18:08:49 -0700

    Clean up fuzz test, and correctly guard against over-archiving

--- rust/protocol/fuzz/fuzz_targets/interaction.rs
+++ rust/protocol/fuzz/fuzz_targets/interaction.rs
@@ -13,178 +13,169 @@ use libsignal_protocol::*;
 use log::*;
 use rand::prelude::*;
 
-async fn process_pre_key(
-    my_name: &str,
-    my_store: &mut InMemSignalProtocolStore,
-    their_store: &mut InMemSignalProtocolStore,
-    their_address: &ProtocolAddress,
-    use_one_time_pre_key: bool,
-    rng: &mut (impl Rng + CryptoRng),
-) {
-    info!("{}:   processing a new pre-key bundle", my_name);
-    let their_signed_pre_key_pair = KeyPair::generate(rng);
-    let their_signed_pre_key_public = their_signed_pre_key_pair.public_key.serialize();
-    let their_signed_pre_key_signature = their_store
-        .get_identity_key_pair(None)
-        .await
-        .unwrap()
-        .private_key()
-        .calculate_signature(&their_signed_pre_key_public, rng)
-        .unwrap();
+struct Participant {
+    name: &'static str,
+    address: ProtocolAddress,
+    store: InMemSignalProtocolStore,
+    message_queue: Vec<(CiphertextMessage, Box<[u8]>)>,
+    archive_count: u8,
+}
 
-    let signed_pre_key_id = rng.gen_range(0, 0xFF_FFFF);
+impl Participant {
+    async fn process_pre_key(
+        &mut self,
+        them: &mut Self,
+        use_one_time_pre_key: bool,
+        rng: &mut (impl Rng + CryptoRng),
+    ) {
+        info!("{}:   processing a new pre-key bundle", self.name);
+        let their_signed_pre_key_pair = KeyPair::generate(rng);
+        let their_signed_pre_key_public = their_signed_pre_key_pair.public_key.serialize();
+        let their_signed_pre_key_signature = them
+            .store
+            .get_identity_key_pair(None)
+            .await
+            .unwrap()
+            .private_key()
+            .calculate_signature(&their_signed_pre_key_public, rng)
+            .unwrap();
 
-    their_store
-        .save_signed_pre_key(
-            signed_pre_key_id,
-            &SignedPreKeyRecord::new(
+        let signed_pre_key_id = rng.gen_range(0, 0xFF_FFFF);
+
+        them.store
+            .save_signed_pre_key(
                 signed_pre_key_id,
-                /*timestamp*/ 42,
-                &their_signed_pre_key_pair,
-                &their_signed_pre_key_signature,
-            ),
-            None,
-        )
-        .await
-        .unwrap();
-
-    let pre_key_info = if use_one_time_pre_key {
-        let pre_key_id = rng.gen_range(0, 0xFF_FFFF);
-        let one_time_pre_key = KeyPair::generate(rng);
-
-        their_store
-            .save_pre_key(
-                pre_key_id,
-                &PreKeyRecord::new(pre_key_id, &one_time_pre_key),
+                &SignedPreKeyRecord::new(
+                    signed_pre_key_id,
+                    /*timestamp*/ 42,
+                    &their_signed_pre_key_pair,
+                    &their_signed_pre_key_signature,
+                ),
                 None,
             )
             .await
             .unwrap();
-        Some((pre_key_id, one_time_pre_key.public_key))
-    } else {
-        None
-    };
 
-    let their_pre_key_bundle = PreKeyBundle::new(
-        their_store.get_local_registration_id(None).await.unwrap(),
-        1, // device id
-        pre_key_info,
-        signed_pre_key_id,
-        their_signed_pre_key_pair.public_key,
-        their_signed_pre_key_signature.into_vec(),
-        *their_store
-            .get_identity_key_pair(None)
-            .await
-            .unwrap()
-            .identity_key(),
-    )
-    .unwrap();
+        let pre_key_info = if use_one_time_pre_key {
+            let pre_key_id = rng.gen_range(0, 0xFF_FFFF);
+            let one_time_pre_key = KeyPair::generate(rng);
 
-    process_prekey_bundle(
-        their_address,
-        &mut my_store.session_store,
-        &mut my_store.identity_store,
-        &their_pre_key_bundle,
-        rng,
-        None,
-    )
-    .await
-    .unwrap();
-}
+            them.store
+                .save_pre_key(
+                    pre_key_id,
+                    &PreKeyRecord::new(pre_key_id, &one_time_pre_key),
+                    None,
+                )
+                .await
+                .unwrap();
+            Some((pre_key_id, one_time_pre_key.public_key))
+        } else {
+            None
+        };
 
-async fn send_message(
-    my_name: &str,
-    my_store: &mut InMemSignalProtocolStore,
-    their_store: &mut InMemSignalProtocolStore,
-    their_address: &ProtocolAddress,
-    their_message_queue: &mut Vec<(CiphertextMessage, Box<[u8]>)>,
-    rng: &mut (impl Rng + CryptoRng),
-) {
-    info!("{}: sending message", my_name);
-    if my_store
-        .load_session(their_address, None)
-        .await
-        .unwrap()
-        .map(|session| !session.has_current_session_state())
-        .unwrap_or(true)
-    {
-        process_pre_key(
-            my_name,
-            my_store,
-            their_store,
-            their_address,
-            rng.gen_bool(0.75),
-            rng,
+        let their_pre_key_bundle = PreKeyBundle::new(
+            them.store.get_local_registration_id(None).await.unwrap(),
+            1, // device id
+            pre_key_info,
+            signed_pre_key_id,
+            their_signed_pre_key_pair.public_key,
+            their_signed_pre_key_signature.into_vec(),
+            *them
+                .store
+                .get_identity_key_pair(None)
+                .await
+                .unwrap()
+                .identity_key(),
         )
-        .await;
-    }
+        .unwrap();
 
-    let length = rng.gen_range(0, 140);
-    let mut buffer = vec![0; length];
-    rng.fill_bytes(&mut buffer);
-
-    let outgoing_message = message_encrypt(
-        &buffer,
-        their_address,
-        &mut my_store.session_store,
-        &mut my_store.identity_store,
-        None,
-    )
-    .await
-    .unwrap();
-
-    // Test serialization ahead of time.
-    let incoming_message = match outgoing_message.message_type() {
-        CiphertextMessageType::PreKey => CiphertextMessage::PreKeySignalMessage(
-            PreKeySignalMessage::try_from(outgoing_message.serialize()).unwrap(),
-        ),
-        CiphertextMessageType::Whisper => CiphertextMessage::SignalMessage(
-            SignalMessage::try_from(outgoing_message.serialize()).unwrap(),
-        ),
-        other_type => panic!("unexpected type {:?}", other_type),
-    };
-
-    their_message_queue.push((incoming_message, buffer.into()));
-}
-
-async fn receive_messages(
-    my_name: &str,
-    my_store: &mut InMemSignalProtocolStore,
-    my_message_queue: &mut Vec<(CiphertextMessage, Box<[u8]>)>,
-    their_address: &ProtocolAddress,
-    rng: &mut (impl Rng + CryptoRng),
-) {
-    info!("{}: receiving messages", my_name);
-    for (incoming_message, expected) in my_message_queue.drain(..) {
-        let decrypted = message_decrypt(
-            &incoming_message,
-            their_address,
-            &mut my_store.session_store,
-            &mut my_store.identity_store,
-            &mut my_store.pre_key_store,
-            &mut my_store.signed_pre_key_store,
+        process_prekey_bundle(
+            &them.address,
+            &mut self.store.session_store,
+            &mut self.store.identity_store,
+            &their_pre_key_bundle,
             rng,
             None,
         )
         .await
         .unwrap();
-
-        assert_eq!(expected, decrypted.into());
     }
-}
 
-async fn archive_session(
-    my_name: &str,
-    my_store: &mut InMemSignalProtocolStore,
-    their_address: &ProtocolAddress,
-) {
-    if let Some(mut session) = my_store.load_session(their_address, None).await.unwrap() {
-        info!("{}: archiving session", my_name);
-        session.archive_current_state().unwrap();
-        my_store
-            .store_session(their_address, &session, None)
+    async fn send_message(&mut self, them: &mut Self, rng: &mut (impl Rng + CryptoRng)) {
+        info!("{}: sending message", self.name);
+        if self
+            .store
+            .load_session(&them.address, None)
             .await
             .unwrap()
+            .map(|session| !session.has_current_session_state())
+            .unwrap_or(true)
+        {
+            self.process_pre_key(them, rng.gen_bool(0.75), rng).await;
+        }
+
+        let length = rng.gen_range(0, 140);
+        let mut buffer = vec![0; length];
+        rng.fill_bytes(&mut buffer);
+
+        let outgoing_message = message_encrypt(
+            &buffer,
+            &them.address,
+            &mut self.store.session_store,
+            &mut self.store.identity_store,
+            None,
+        )
+        .await
+        .unwrap();
+
+        // Test serialization ahead of time.
+        let incoming_message = match outgoing_message.message_type() {
+            CiphertextMessageType::PreKey => CiphertextMessage::PreKeySignalMessage(
+                PreKeySignalMessage::try_from(outgoing_message.serialize()).unwrap(),
+            ),
+            CiphertextMessageType::Whisper => CiphertextMessage::SignalMessage(
+                SignalMessage::try_from(outgoing_message.serialize()).unwrap(),
+            ),
+            other_type => panic!("unexpected type {:?}", other_type),
+        };
+
+        them.message_queue.push((incoming_message, buffer.into()));
+    }
+
+    async fn receive_messages(
+        &mut self,
+        their_address: &ProtocolAddress,
+        rng: &mut (impl Rng + CryptoRng),
+    ) {
+        info!("{}: receiving messages", self.name);
+        for (incoming_message, expected) in self.message_queue.drain(..) {
+            let decrypted = message_decrypt(
+                &incoming_message,
+                their_address,
+                &mut self.store.session_store,
+                &mut self.store.identity_store,
+                &mut self.store.pre_key_store,
+                &mut self.store.signed_pre_key_store,
+                rng,
+                None,
+            )
+            .await
+            .unwrap();
+
+            assert_eq!(expected, decrypted.into());
+        }
+    }
+
+    async fn archive_session(&mut self, their_address: &ProtocolAddress) {
+        if let Some(mut session) = self.store.load_session(their_address, None).await.unwrap() {
+            info!("{}: archiving session", self.name);
+            session.archive_current_state().unwrap();
+            self.store
+                .store_session(their_address, &session, None)
+                .await
+                .unwrap();
+            self.archive_count += 1;
+        }
     }
 }
 
@@ -195,72 +186,57 @@ fuzz_target!(|data: (u64, &[u8])| {
     async {
         let mut csprng = StdRng::seed_from_u64(seed);
 
-        let alice_address = ProtocolAddress::new("+14151111111".to_owned(), 1);
-        let bob_address = ProtocolAddress::new("+14151111112".to_owned(), 1);
-
-        let mut alice_store =
-            InMemSignalProtocolStore::new(IdentityKeyPair::generate(&mut csprng), csprng.gen())
-                .unwrap();
-        let mut bob_store =
-            InMemSignalProtocolStore::new(IdentityKeyPair::generate(&mut csprng), csprng.gen())
-                .unwrap();
-
-        let mut alice_queue = Vec::new();
-        let mut bob_queue = Vec::new();
+        let mut alice = Participant {
+            name: "alice",
+            address: ProtocolAddress::new("+14151111111".to_owned(), 1),
+            store: InMemSignalProtocolStore::new(
+                IdentityKeyPair::generate(&mut csprng),
+                csprng.gen(),
+            )
+            .unwrap(),
+            message_queue: Vec::new(),
+            archive_count: 0,
+        };
+        let mut bob = Participant {
+            name: "bob",
+            address: ProtocolAddress::new("+14151111112".to_owned(), 1),
+            store: InMemSignalProtocolStore::new(
+                IdentityKeyPair::generate(&mut csprng),
+                csprng.gen(),
+            )
+            .unwrap(),
+            message_queue: Vec::new(),
+            archive_count: 0,
+        };
 
         for action in actions {
-            let (my_name, my_store, my_queue, their_store, their_address, their_queue) =
-                match action & 1 {
-                    0 => (
-                        "alice",
-                        &mut alice_store,
-                        &mut alice_queue,
-                        &mut bob_store,
-                        &bob_address,
-                        &mut bob_queue,
-                    ),
-                    1 => (
-                        "bob",
-                        &mut bob_store,
-                        &mut bob_queue,
-                        &mut alice_store,
-                        &alice_address,
-                        &mut alice_queue,
-                    ),
-                    _ => unreachable!(),
-                };
+            let (me, them) = match action & 1 {
+                0 => (&mut alice, &mut bob),
+                1 => (&mut bob, &mut alice),
+                _ => unreachable!(),
+            };
             match action >> 1 {
                 0 => {
-                    if my_queue.len() < 40 && their_queue.len() < 40 {
+                    if me.archive_count < 40 {
                         // Only archive if it can't result in old sessions getting expired.
                         // We're not testing that.
-                        archive_session(my_name, my_store, their_address).await
+                        me.archive_session(&them.address).await
                     }
-                },
-                1..=32 => {
-                    receive_messages(my_name, my_store, my_queue, their_address, &mut csprng).await
                 }
+                1..=32 => me.receive_messages(&them.address, &mut csprng).await,
                 33..=48 => {
-                    info!("{}: drop an incoming message", my_name);
-                    my_queue.pop();
+                    info!("{}: drop an incoming message", me.name);
+                    me.message_queue.pop();
                 }
                 49..=56 => {
-                    info!("{}: shuffle incoming messages", my_name);
-                    my_queue.shuffle(&mut csprng);
+                    info!("{}: shuffle incoming messages", me.name);
+                    me.message_queue.shuffle(&mut csprng);
                 }
                 _ => {
-                    if their_queue.len() < 1_500 {
+                    if them.message_queue.len() < 1_500 {
                         // Only send if it can't result in a too-long chain.
                         // We're not testing that.
-                        send_message(
-                            my_name,
-                            my_store,
-                            their_store,
-                            their_address,
-                            their_queue,
-                            &mut csprng,
-                        )
-                        .await
+                        me.send_message(them, &mut csprng).await
                     }
                 }
             }
