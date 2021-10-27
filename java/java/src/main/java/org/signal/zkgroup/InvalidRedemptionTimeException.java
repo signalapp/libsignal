b8943aef2eed3599167d1b1456a3b1e99ee1dd16
Jordan Rose <jrose@signal.org>
2021-10-27 12:09:58 -0700

    java: Update zkgroup license headers, remove codegen warnings

--- java/java/src/main/java/org/signal/zkgroup/InvalidRedemptionTimeException.java
+++ java/java/src/main/java/org/signal/zkgroup/InvalidRedemptionTimeException.java
@@ -1,3 +1,8 @@
+//
+// Copyright 2021 Signal Messenger, LLC.
+// SPDX-License-Identifier: AGPL-3.0-only
+//
+
 package org.signal.zkgroup;
 
 public class InvalidRedemptionTimeException extends Exception {
