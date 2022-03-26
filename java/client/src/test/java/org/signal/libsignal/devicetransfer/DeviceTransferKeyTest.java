48ce4bf27dcf353cb39af54b49712d8e4dec2805
Jordan Rose <jrose@signal.org>
2022-03-25 17:21:12 -0700

    java: Test devicetransfer with java.security.cert.CertificateFactory
    ...instead of the deprecated javax.security.cert.X509Certificate.

--- java/client/src/test/java/org/signal/libsignal/devicetransfer/DeviceTransferKeyTest.java
+++ java/client/src/test/java/org/signal/libsignal/devicetransfer/DeviceTransferKeyTest.java
@@ -1,18 +1,21 @@
 //
-// Copyright 2021 Signal Messenger, LLC.
+// Copyright 2021-2022 Signal Messenger, LLC.
 // SPDX-License-Identifier: AGPL-3.0-only
 //
 
 package org.signal.libsignal.devicetransfer;
 
 import junit.framework.TestCase;
-import javax.security.cert.X509Certificate;
+
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateFactory;
 
 public class DeviceTransferKeyTest extends TestCase {
   public void testDeviceTransferKey() throws Exception {
     DeviceTransferKey key = new DeviceTransferKey();
     byte[] certBytes = key.generateCertificate("name", 365);
 
-    X509Certificate cert = X509Certificate.getInstance(certBytes);
+    CertificateFactory cf = CertificateFactory.getInstance("X.509");
+    cf.generateCertificate(new ByteArrayInputStream(certBytes));
   }
 }
