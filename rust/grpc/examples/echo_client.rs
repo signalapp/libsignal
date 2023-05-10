f1993964b5e64b2357ce60490bbee65ef9fe5181
Joeri Sykora <joeri@sertik.net>
2023-05-10 09:01:57 +0200

    use default target

--- rust/grpc/examples/echo_client.rs
+++ rust/grpc/examples/echo_client.rs
@@ -3,8 +3,7 @@ use signal_grpc::GrpcClient;
 use signal_grpc::Result;
 
 fn main() -> Result<()> {
-    let mut grpc_client = GrpcClient::new()?;
-    grpc_client.target("https://grpcproxy.gluonhq.net:443");
+    let grpc_client = GrpcClient::new()?;
 
     let reply = grpc_client.echo_message("PING")?;
 
