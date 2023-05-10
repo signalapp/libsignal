6f810851d44bedcbb9218378f185fe94c64145ea
Joeri Sykora <joeri@sertik.net>
2023-05-10 08:58:26 +0200

    echo message to proxy server

--- rust/grpc/examples/echo_client.rs
+++ rust/grpc/examples/echo_client.rs
@@ -4,7 +4,7 @@ use signal_grpc::Result;
 
 fn main() -> Result<()> {
     let mut grpc_client = GrpcClient::new()?;
-    grpc_client.target("http://localhost:50051");
+    grpc_client.target("https://grpcproxy.gluonhq.net:443");
 
     let reply = grpc_client.echo_message("PING")?;
 
