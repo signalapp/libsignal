d952869106f6d0e47597d1ed1c2219127a0a4465
Jordan Rose <jrose@signal.org>
2023-12-13 16:12:30 -0800

    CI: Include the cargo-about version in the local tools cache key
    GitHub was restoring the 0.5.7 cache, and then not updating the cache
    after installing 0.6.0 because it was successfully restored.

--- bin/regenerate_acknowledgments.sh
+++ bin/regenerate_acknowledgments.sh
@@ -7,19 +7,19 @@
 
 set -euo pipefail
 
+SCRIPT_DIR=$(dirname "$0")
+cd "${SCRIPT_DIR}"/..
+. bin/build_helpers.sh
+
 echo "Checking cargo-about version"
 VERSION=$(cargo about --version)
 echo "Found $VERSION"
 
-EXPECTED_VERSION="cargo-about 0.6.0"
+EXPECTED_VERSION="cargo-about $(cat acknowledgments/cargo-about-version)"
 if [ "$VERSION" != "$EXPECTED_VERSION" ]; then
 	echo "This tool works with $EXPECTED_VERSION but $VERSION is installed"
 	false
 fi
-
-SCRIPT_DIR=$(dirname "$0")
-cd "${SCRIPT_DIR}"/..
-. bin/build_helpers.sh
 
 for template in acknowledgments/*.hbs; do
     echo_then_run cargo about generate --config acknowledgments/about.toml --all-features --fail "$template" --output-file "${template%.hbs}"
