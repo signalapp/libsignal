a5a2b42046ac4ec5b6b7e82b8b9b61e410022021
Jordan Rose <jrose@signal.org>
2024-01-10 16:14:40 -0800

    Node: Specifically build with prebuildify@^5
    prebuildify 6.0.0, just released, changed the name of the built
    module, which our locked version of node-gyp-build then couldn't find.
    By specifying a version in package.json and making sure we always
    `yarn install` before running prebuildify, we'll use the version from
    our lockfile, and never accidentally update to a new major version.

--- node/docker-prebuildify.sh
+++ node/docker-prebuildify.sh
@@ -22,8 +22,10 @@ docker build --build-arg "UID=${UID:-501}" --build-arg "GID=${GID:-501}" -t ${DO
 
 # We build both architectures in the same run action to save on intermediates
 # (including downloading dependencies)
+# We run `yarn install` to make sure the correct prebuildify version is used.
 docker run ${IS_TTY:+ -it} --init --rm -v "${PWD}":/home/libsignal/src ${DOCKER_IMAGE} sh -c '
     cd ~/src/node &&
+    npx yarn install --ignore-scripts --frozen-lockfile &&
     env CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc \
         CC=aarch64-linux-gnu-gcc \
         CXX=aarch64-linux-gnu-g++ \
