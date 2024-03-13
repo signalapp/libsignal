6b253b5ddbd78338a902f7bcdf200253fe632936
Alex Konradi <akonradi@signal.org>
2024-03-13 11:22:44 -0400

    Continue the size check if GH response is invalid
    Continue the rest of the check_code_size.py script if the GH tool invoked by the
    script fails or produces unexpected results.

--- java/check_code_size.py
+++ java/check_code_size.py
@@ -59,11 +59,13 @@ def current_origin_main_entry():
                 (bytes_count, _) = after.split(" ", maxsplit=1)
                 return {'size': int(bytes_count), 'version': most_recent_main[:6] + ' (main)'}
 
-        return None
-    except subprocess.CalledProcessError as e:
-        print("not checking current origin/main:", e, file=sys.stderr)
-        print("stdout:", e.stdout.decode(), file=sys.stderr)
-        print("stderr:", e.stderr.decode(), file=sys.stderr)
+    except Exception as e:
+        print("skipping checking current origin/main:", e, file=sys.stderr)
+        if isinstance(e, subprocess.CalledProcessError):
+            print("stdout:", e.stdout.decode(), file=sys.stderr)
+            print("stderr:", e.stderr.decode(), file=sys.stderr)
+
+    return None
 
 
 our_abs_dir = os.path.dirname(os.path.realpath(__file__))
