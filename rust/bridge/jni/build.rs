9d729ff08397a2279933e2e86feeb3f911f30b43
Jordan Rose <jrose@signal.org>
2024-07-25 14:22:56 -0700

    Re-apply "android: Don't strip debug info out of the AAR"
    A revised version of the earlier 23192280:
    - Uses --build-id for all archs, not just aarch64
    - Also raises Gradle's memory limits

--- rust/bridge/jni/build.rs
+++ rust/bridge/jni/build.rs
@@ -6,12 +6,15 @@
 use std::env;
 
 fn main() {
-    if env::var("CARGO_CFG_TARGET_ARCH").expect("set by Cargo") == "aarch64"
-        && env::var("CARGO_CFG_TARGET_OS").expect("set by Cargo") == "android"
-    {
-        // HACK: Force libdl to be linked.
-        // Something about the Docker-based build results in it getting skipped;
-        // if we figure out what, we can remove this.
-        println!("cargo:rustc-cdylib-link-arg=-Wl,--no-as-needed,-ldl,--as-needed");
+    if env::var("CARGO_CFG_TARGET_OS").expect("set by Cargo") == "android" {
+        // --build-id ensures that Android Studio's LLDB can map stripped binaries back to their debug info
+        println!("cargo:rustc-cdylib-link-arg=-Wl,--build-id");
+
+        if env::var("CARGO_CFG_TARGET_ARCH").expect("set by Cargo") == "aarch64" {
+            // HACK: Force libdl to be linked.
+            // Something about the Docker-based build results in it getting skipped;
+            // if we figure out what, we can remove this.
+            println!("cargo:rustc-cdylib-link-arg=-Wl,--no-as-needed,-ldl,--as-needed");
+        }
     }
 }
