6b5b0429c92bdec0852c82e0c11472d5e00a5df7
moiseev-signal <122060238+moiseev-signal@users.noreply.github.com>
2024-09-13 16:37:50 -0700

    Add mac_setup script.

---
+++ bin/mac_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#set -ex
+
+cat << EOF | brew bundle install --file=-
+tap "homebrew/bundle"
+brew "awscli"
+brew "cmake"
+brew "coreutils"
+brew "fnm"
+brew "git"
+brew "jq"
+brew "just"
+brew "protobuf"
+brew "python"
+brew "rocksdb"
+brew "ruby"
+brew "rustup"
+brew "shellcheck"
+brew "swiftformat"
+brew "swiftlint"
+brew "taplo"
+brew "terraform"
+brew "yamllint"
+cask "google-cloud-sdk"
+EOF
