86e5e1d60e8d25ea6f6725744e704aae55440eff
andrew-signal <andrew@signal.org>
2025-01-09 12:59:40 -0500

    Update smoke test to use ChatConnection rather than ChatService
    Co-authored-by: Alex Konradi <akonradi@signal.org>

--- rust/net/infra/src/route/provider.rs
+++ rust/net/infra/src/route/provider.rs
@@ -5,14 +5,37 @@
 
 use crate::route::{RouteProvider, RouteProviderContext};
 
+/// Additional methods available for [`RouteProvider`]s.
+///
+/// Provides utility combinators for `RouteProvider`s, similar to
+/// [`std::iter::Iterator`].
 pub trait RouteProviderExt: RouteProvider + Sized {
+    /// Returns a new [`RouteProvider`] that transforms routes.
+    ///
+    /// Consumes an existing route provider and returns a new one that returns
+    /// the result of invoking the provided callback on each route produced by
+    /// the original provider.
+    ///
+    /// This is analagous to [`Iterator::map`] for iterators.
     fn map_routes<F: Fn(Self::Route) -> T, T>(self, f: F) -> Map<Self, F> {
         Map(self, f)
+    }
+
+    /// Returns a new [`RouteProvider`] that filters routes.
+    ///
+    /// Consumes an existing route provider and returns a new one that filters
+    /// the output of the original by only returning routes for which the
+    /// provided callback returns `true`.
+    ///
+    /// This is analagous to [`Iterator::filter`] for iterators.
+    fn filter_routes<F: Fn(&Self::Route) -> bool>(self, f: F) -> Filter<Self, F> {
+        Filter(self, f)
     }
 }
 
 impl<R: RouteProvider> RouteProviderExt for R {}
 
+/// The [`RouteProvider`] returned by [`RouteProviderExt::map_routes`].
 pub struct Map<R, F>(R, F);
 
 impl<R: RouteProvider, F: Fn(R::Route) -> T, T> RouteProvider for Map<R, F> {
@@ -23,5 +46,16 @@ impl<R: RouteProvider, F: Fn(R::Route) -> T, T> RouteProvider for Map<R, F> {
         context: &impl RouteProviderContext,
     ) -> impl Iterator<Item = Self::Route> + 's {
         self.0.routes(context).map(&self.1)
+    }
+}
+
+/// The [`RouteProvider`] returned by [`RouteProviderExt::filter_routes`].
+pub struct Filter<R, F>(R, F);
+
+impl<R: RouteProvider, F: Fn(&R::Route) -> bool> RouteProvider for Filter<R, F> {
+    type Route = R::Route;
+
+    fn routes(&self) -> impl Iterator<Item = Self::Route> + '_ {
+        self.0.routes().filter(&self.1)
     }
 }
