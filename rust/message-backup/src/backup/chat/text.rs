fec7839e2b7791462e88a893054fc4020168bec6
Jordan Rose <jrose@signal.org>
2025-01-22 11:59:18 -0800

    backup: Be stricter about MessageText
    - No empty bodies
    - Update Backup.proto to make BodyRange.start+length non-optional

--- rust/message-backup/src/backup/chat/text.rs
+++ rust/message-backup/src/backup/chat/text.rs
@@ -20,8 +20,8 @@ pub struct MessageText {
 #[derive(Debug, serde::Serialize)]
 #[cfg_attr(test, derive(PartialEq, Clone))]
 pub struct TextRange {
-    pub start: Option<u32>,
-    pub length: Option<u32>,
+    pub start: u32,
+    pub length: u32,
     pub effect: TextEffect,
 }
 
@@ -35,6 +35,8 @@ pub enum TextEffect {
 #[derive(Debug, displaydoc::Display, thiserror::Error)]
 #[cfg_attr(test, derive(PartialEq))]
 pub enum TextError {
+    /// body was empty
+    EmptyBody,
     /// mention had invalid ACI
     MentionInvalidAci,
     /// BodyRange.associatedValue is a oneof but has no value
@@ -50,6 +52,10 @@ impl TryFrom<proto::Text> for MessageText {
             bodyRanges,
             special_fields: _,
         } = value;
+
+        if body.is_empty() {
+            return Err(TextError::EmptyBody);
+        }
 
         let ranges = likely_empty(bodyRanges, |iter| {
             iter.map(|range| {
@@ -84,6 +90,8 @@ impl TryFrom<proto::Text> for MessageText {
 
 #[cfg(test)]
 mod test {
+    use test_case::test_case;
+
     use super::*;
     use crate::backup::testutil::TEST_MESSAGE_TEXT;
 
@@ -92,8 +100,8 @@ mod test {
             Self {
                 body: TEST_MESSAGE_TEXT.to_string(),
                 bodyRanges: vec![proto::BodyRange {
-                    start: Some(2),
-                    length: Some(5),
+                    start: 2,
+                    length: 5,
                     associatedValue: Some(proto::body_range::AssociatedValue::Style(
                         proto::body_range::Style::MONOSPACE.into(),
                     )),
@@ -109,8 +117,8 @@ mod test {
             Self {
                 text: TEST_MESSAGE_TEXT.to_string(),
                 ranges: vec![TextRange {
-                    start: Some(2),
-                    length: Some(5),
+                    start: 2,
+                    length: 5,
                     effect: TextEffect::Style(proto::body_range::Style::MONOSPACE),
                 }]
                 .into(),
@@ -126,16 +134,36 @@ mod test {
         );
     }
 
+    #[test_case(|x| x.body = "".into() => Err(TextError::EmptyBody); "empty body")]
+    #[test_case(|x| x.bodyRanges.push(Default::default()) => Err(TextError::NoAssociatedValueForBodyRange); "invalid body range")]
+    #[test_case(|x| {
+        x.bodyRanges.push(proto::BodyRange {
+            associatedValue: Some(proto::body_range::AssociatedValue::MentionAci(vec![])),
+            ..Default::default()
+        });
+    } => Err(TextError::MentionInvalidAci); "invalid mention")]
+    #[test_case(|x| {
+        x.bodyRanges.push(proto::BodyRange {
+            associatedValue: Some(proto::body_range::AssociatedValue::MentionAci(proto::Contact::TEST_ACI.to_vec())),
+            ..Default::default()
+        });
+    } => Ok(()); "valid mention")]
+    fn text(modifier: fn(&mut proto::Text)) -> Result<(), TextError> {
+        let mut message = proto::Text::test_data();
+        modifier(&mut message);
+        message.try_into().map(|_: MessageText| ())
+    }
+
     #[test]
     fn ranges_are_sorted_when_serialized() {
         let range1 = TextRange {
-            start: Some(2),
-            length: Some(5),
+            start: 2,
+            length: 5,
             effect: TextEffect::Style(proto::body_range::Style::MONOSPACE),
         };
         let range2 = TextRange {
-            start: Some(10),
-            length: Some(2),
+            start: 10,
+            length: 2,
             effect: TextEffect::Style(proto::body_range::Style::BOLD),
         };
 
