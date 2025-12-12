46306b2b6b6920bf2c2d3977f355bb2eebb58430
Jordan Rose <jrose@signal.org>
2025-12-12 13:23:47 -0800

    net: Remove now-unused timeout constants

--- rust/net/infra/src/timeouts.rs
+++ rust/net/infra/src/timeouts.rs
@@ -51,36 +51,12 @@ pub const NETWORK_INTERFACE_POLL_INTERVAL: Duration = Duration::from_secs(5);
 /// interface.
 pub const POST_ROUTE_CHANGE_CONNECTION_TIMEOUT: Duration = Duration::from_secs(1);
 
-/// Timeout for a connect operation that attempts multiple routes
-pub const MULTI_ROUTE_CONNECTION_TIMEOUT: Duration = Duration::from_secs(180);
-
-/// When establishing a TCP connection, connections to different IP addresses are
-/// raced between each other with each new attempt being given an additional delay
-/// before it starts.
-pub const TCP_CONNECTION_ATTEMPT_DELAY: Duration = Duration::from_millis(200);
-
 /// Timeout for a TCP connection attempt to a single IP address.
 pub const TCP_CONNECTION_TIMEOUT: Duration = Duration::from_secs(15);
 
 /// Minimum timeout duration for TLS handshake. May be greater depending on length of
 /// the TCP handshake.
 pub const MIN_TLS_HANDSHAKE_TIMEOUT: Duration = Duration::from_secs(3);
-
-/// A sequence of timeout values to be used as cooldown intervals between attempts
-/// when a connection to a given route is consecutively failing to establish
-pub const CONNECTION_ROUTE_COOLDOWN_INTERVALS: [Duration; 8] = [
-    Duration::from_secs(0),
-    Duration::from_secs(1),
-    Duration::from_secs(2),
-    Duration::from_secs(4),
-    Duration::from_secs(8),
-    Duration::from_secs(16),
-    Duration::from_secs(32),
-    CONNECTION_ROUTE_MAX_COOLDOWN,
-];
-
-/// Maximum value of a coolduwn interval between connection attempts
-pub const CONNECTION_ROUTE_MAX_COOLDOWN: Duration = Duration::from_secs(64);
 
 /// The result of an operation that can time out or produce a value.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, derive_more::From)]
