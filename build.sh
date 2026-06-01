ed090f86d6305181435b26772846c282dd8625b6
21pchangkachith <67136281+21pchangkachith@users.noreply.github.com>
2026-05-31 20:48:51 -0700

    remove/edit dev scripts

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+# This is what our team needed to do to let Desktop use the rust code.
+# We preserve this for anyone else who will encounter this problem
 cargo build
 nvm use
 cd node
-#npm install
 npm run build
 npm run tsc
-# cp -b ./prebuilds/darwin-arm64/electron.abi140.node ./backups/electron.abi140.node.bk
-# rm ./prebuilds/darwin-arm64/electron.abi140.node
 mkdir prebuilds
 mkdir prebuilds/darwin-arm64
 cp ./build/Release/libsignal_client_darwin_arm64.node ./prebuilds/darwin-arm64/electron.abi140.node
